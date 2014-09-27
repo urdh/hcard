e8cf637d905bac25dda5a395c3c9f866f6f8e57f
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2014-09-27 22:14:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# hCard
+
+This is an app serving my hCard at [sigurdhsson.org](http://sigurdhsson.org).
+In addition to doing that, it serves 301/410 messages on some URLs, since some old things are now gone and some (including [my blog](http://blog.sigurdhsson.org) and [Webboken](http://webboken.github.io)) have moved permanently.
+
+License is MIT, if anyone cares.
---
+++ index.js
@@ -0,0 +1,90 @@
+var app = require('koa')();
+var staticCache = require('koa-static-cache');
+
+var pathToRegexp = require('path-to-regexp');
+var path = require('path');
+var fs = require('mz/fs');
+
+// First, some "top-layer" middlewares
+app.use(require('koa-helmet').hidePoweredBy());
+app.use(require('koa-conditional-get')());
+app.use(require('koa-etag')());
+app.use(require('koa-compress')());
+
+// Then we handle errors
+app.use(function *(next) {
+  try {
+    yield next;
+    if(this.status == 404) {
+      this.body = fs.readFileSync(path.join(__dirname, 'errors', '404.html'));
+      this.type = 'html';
+      this.status = 404;
+    }
+    if(this.status == 410) {
+      this.body = fs.readFileSync(path.join(__dirname, 'errors', '410.html'));
+      this.type = 'html';
+      this.status = 410;
+    }
+  } catch (err) {
+    this.status = err.status || 500;
+    if(this.status == 500) {
+      this.body = fs.readFileSync(path.join(__dirname, 'errors', '500.html'));
+      this.type = 'html';
+      this.status = 500;
+    }
+    this.app.emit('error', err, this);
+  }
+});
+
+// Then, our route middlewares for redirects and missing pages
+function gone(path) {
+  return function*(next) {
+    var re = pathToRegexp(path);
+    if(re.exec(this.path)) {
+      this.status = 410;
+    } else {
+      yield next;
+    }
+  }
+}
+function moved(path, target) {
+  return function *(next){
+    var re = pathToRegexp(path);
+    if(m = re.exec(this.path)) {
+      this.set('Location', this.path.replace(re, target));
+      this.status = 301;
+    } else {
+      yield next;
+    }
+  }
+}
+// These are gone forever
+app.use(gone('/archives/*'));
+app.use(gone('/portfolio/*'));
+app.use(gone('/autobrew'));
+app.use(gone('/chslacite'));
+app.use(gone('/posts/I-X/*'));
+// These are moved
+app.use(moved('/webboken/v2/:uri*', 'http://webboken.github.io/$1'));
+app.use(moved('/media/projects/latexbok/latexbok.pdf',
+  'http://github.com/urdh/latexbok/releases/download/edition-2/latexbok-a4.pdf'));
+app.use(moved('/latexbok/media/latexbok.pdf',
+  'http://github.com/urdh/latexbok/releases/download/edition-2/latexbok-a4.pdf'));
+app.use(moved('/projects/latexhax.html', 'http://blog.sigurdhsson.org/latexhax.html'));
+// These are on the current blog
+app.use(moved('/sitemap.xml',   'http://blog.sigurdhsson.org/sitemap.xml'));
+app.use(moved('/atom.xml',      'http://blog.sigurdhsson.org/atom.xml'));
+app.use(moved('/2012/11/:post', 'http://blog.sigurdhsson.org/2012/11/$1'));
+app.use(moved('/2014/04/:post', 'http://blog.sigurdhsson.org/2014/04/$1'));
+app.use(moved('/2014/09/:post', 'http://blog.sigurdhsson.org/2014/09/$1'));
+
+// Finally, the static cache serving middleware serving the hCard
+app.use(staticCache(path.join(__dirname, 'public'), {
+  maxAge: 28 * 24 * 60 * 60,
+  buffer: true,
+  gzip: false, // compress middleware does this
+  alias: { '/': '/index.html' }
+}));
+
+// And run the application
+if (!module.parent) app.listen(process.env.PORT || 5000);
